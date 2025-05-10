c3ec99e6ebfc04fb4ef25eebe35227ebea614440
Misha Anisimov <158746523+Anisimov-Misha@users.noreply.github.com>
2025-05-10 18:20:03 +0300

    Update Form1.cs

--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
-namespace Lab7CSharp
+namespace CalculatorApp
 {
     public partial class Form1 : Form
     {
@@ -17,9 +10,37 @@ namespace Lab7CSharp
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnCalculate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                double num1 = double.Parse(txtNumber1.Text);
+                double num2 = double.Parse(txtNumber2.Text);
+                double result = 0;
 
+                if (rbAdd.Checked)
+                    result = num1 + num2;
+                else if (rbSubtract.Checked)
+                    result = num1 - num2;
+                else if (rbMultiply.Checked)
+                    result = num1 * num2;
+                else if (rbDivide.Checked)
+                {
+                    if (num2 == 0)
+                        throw new DivideByZeroException("Не можна ділити на нуль.");
+                    result = num1 / num2;
+                }
+
+                txtResult.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть коректні числа.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка: " + ex.Message);
+            }
         }
     }
 }
