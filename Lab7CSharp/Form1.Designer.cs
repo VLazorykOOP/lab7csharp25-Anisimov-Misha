cf6d763436a0a1723d0e3b9f70542c6fec3dde27
Misha Anisimov <158746523+Anisimov-Misha@users.noreply.github.com>
2025-05-10 18:19:46 +0300

    Update Form1.Designer.cs

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -1,68 +1,97 @@
-﻿
-namespace Lab7CSharp
+namespace CalculatorApp
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.RadioButton rbAdd;
+        private System.Windows.Forms.RadioButton rbSubtract;
+        private System.Windows.Forms.RadioButton rbMultiply;
+        private System.Windows.Forms.RadioButton rbDivide;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1, label2, label3;
+
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.rbAdd = new System.Windows.Forms.RadioButton();
+            this.rbSubtract = new System.Windows.Forms.RadioButton();
+            this.rbMultiply = new System.Windows.Forms.RadioButton();
+            this.rbDivide = new System.Windows.Forms.RadioButton();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.PaleGreen;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(376, -1);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Lab 7.   C# ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
+
+            // txtNumber1
+            this.txtNumber1.Location = new System.Drawing.Point(120, 30);
+            this.txtNumber1.Size = new System.Drawing.Size(100, 20);
+
+            // txtNumber2
+            this.txtNumber2.Location = new System.Drawing.Point(120, 60);
+            this.txtNumber2.Size = new System.Drawing.Size(100, 20);
+
+            // txtResult
+            this.txtResult.Location = new System.Drawing.Point(120, 90);
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+
+            // Radio Buttons
+            this.rbAdd.Text = "Додати";
+            this.rbAdd.Location = new System.Drawing.Point(250, 30);
+
+            this.rbSubtract.Text = "Відняти";
+            this.rbSubtract.Location = new System.Drawing.Point(250, 50);
+
+            this.rbMultiply.Text = "Множити";
+            this.rbMultiply.Location = new System.Drawing.Point(250, 70);
+
+            this.rbDivide.Text = "Ділити";
+            this.rbDivide.Location = new System.Drawing.Point(250, 90);
+
+            // Button
+            this.btnCalculate.Text = "Розрахувати";
+            this.btnCalculate.Location = new System.Drawing.Point(120, 130);
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+
+            // Labels
+            this.label1.Text = "Число 1:";
+            this.label1.Location = new System.Drawing.Point(30, 30);
+
+            this.label2.Text = "Число 2:";
+            this.label2.Location = new System.Drawing.Point(30, 60);
+
+            this.label3.Text = "Результат:";
+            this.label3.Location = new System.Drawing.Point(30, 90);
+
             // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 386);
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.rbAdd);
+            this.Controls.Add(this.rbSubtract);
+            this.Controls.Add(this.rbMultiply);
+            this.Controls.Add(this.rbDivide);
+            this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Text = "Калькулятор";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
-
